c7e3679d24d0455b51e7047c4ed37f31c9c723ee
houyem ben arbia <benarbia.houyem62@gmail.com>
2025-02-05 16:48:19 +0100

    understanding of loops

---
+++ GettingtoKnowtheLanguage/Java Language Basics/ControlFlowStatements/src/main/java/Test.java
@@ -0,0 +1,16 @@
+class Test {
+    enum Day {
+        MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+    }
+
+    public int calculate(Day d) {
+        return switch (d) {
+            case SATURDAY, SUNDAY -> 0;
+            default -> {
+                int remainingWorkDays = 5 - d.ordinal();
+                yield remainingWorkDays;
+            }
+        };
+    }
+}
+//The Yield Statement
