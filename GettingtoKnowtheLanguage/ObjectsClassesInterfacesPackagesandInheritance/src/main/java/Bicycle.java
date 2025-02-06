2e9b8e19f05c1fec53c63bb6d879df81a858b748
houyem ben arbia <benarbia.houyem62@gmail.com>
2025-02-06 17:57:23 +0100

    packages and annotations

--- GettingtoKnowtheLanguage/ObjectsClassesInterfacesPackagesandInheritance/src/main/java/Bicycle.java
+++ GettingtoKnowtheLanguage/ObjectsClassesInterfacesPackagesandInheritance/src/main/java/Bicycle.java
@@ -1,34 +1,24 @@
 public class Bicycle {
 
-    private int cadence;
-    private int gear;
-    private int speed;
+    // the Bicycle class has three fields
+    public int cadence;
+    public int gear;
+    public int speed;
 
-    //constructeur
+    // the Bicycle class has one constructor
     public Bicycle(int startCadence, int startSpeed, int startGear) {
         gear = startGear;
         cadence = startCadence;
         speed = startSpeed;
     }
 
-    public int getCadence() {
-        return cadence;
-    }
-
+    // the Bicycle class has four methods
     public void setCadence(int newValue) {
         cadence = newValue;
     }
 
-    public int getGear() {
-        return gear;
-    }
-
     public void setGear(int newValue) {
         gear = newValue;
-    }
-
-    public int getSpeed() {
-        return speed;
     }
 
     public void applyBrake(int decrement) {
