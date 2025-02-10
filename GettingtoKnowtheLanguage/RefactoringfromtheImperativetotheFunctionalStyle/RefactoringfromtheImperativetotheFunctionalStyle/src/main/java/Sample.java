81ae949ffd91756ca658ca1724bef720ab094995
houyem ben arbia <benarbia.houyem62@gmail.com>
2025-02-10 12:30:48 +0100

    finishing Getting to Know the Language

---
+++ GettingtoKnowtheLanguage/RefactoringfromtheImperativetotheFunctionalStyle/RefactoringfromtheImperativetotheFunctionalStyle/src/main/java/Sample.java
@@ -0,0 +1,27 @@
+//Sample.java
+import java.nio.file.*;
+
+public class Sample {
+    public static void main(String[] args) {
+        try {
+            final var filePath = "./Sample.java";
+            final var wordOfInterest = "public";
+
+            try (var reader = Files.newBufferedReader(Path.of(filePath))) {
+                String line = "";
+                long count = 0;
+
+                while((line = reader.readLine()) != null) {
+                    if(line.contains(wordOfInterest)) {
+                        count++;
+                    }
+                }
+
+                System.out.println(String.format("Found %d lines with the word %s", count, wordOfInterest));
+            }
+        } catch(Exception ex) {
+            System.out.println("ERROR: " + ex.getMessage());
+        }
+    }
+}
+
