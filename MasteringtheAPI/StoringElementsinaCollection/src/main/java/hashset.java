262d9afe2f357af6a0b18684c503bcda9b894b3e
houyem ben arbia <benarbia.houyem62@gmail.com>
2025-02-11 15:57:33 +0100

    correction of the class

--- MasteringtheAPI/StoringElementsinaCollection/src/main/java/hashset.java
+++ MasteringtheAPI/StoringElementsinaCollection/src/main/java/hashset.java
@@ -2,7 +2,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-public class hashset {
+public class Hashset {
     public static void main(String[] args) {
         List<String> strings = List.of("one", "two", "three", "four", "five", "six");
         Set<String> set = new HashSet<>();  //A HashSet is a collection that implements the Set interface and is backed by a hash table.
